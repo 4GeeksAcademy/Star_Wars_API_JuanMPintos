29f9b685c352e3940b1d1ea7fcc6df9ea4d046bc
Juan M. Pintos <166875657+JuanMPintos@users.noreply.github.com>
2024-09-17 07:58:08 +0000

    dudas

--- src/app.py
+++ src/app.py
@@ -37,32 +37,90 @@ def sitemap():
     return generate_sitemap(app)
 
 
-#OBTENGO TODOS LOS USERS
-@app.route("/users", method=['GET'])
-def user_list():
-    users= User.query.all()
-    all_users=list(map(lambda x: x.serialize(), users))
-    return jsonify(all_users), 200
+
 
 #OBTENGO TODOS LOS PERSONAJES
-@app.route("/personajes", method=['GET'])
+@app.route("/personajes", methods=['GET'])
 def personajes_list():
     personajes= Personajes.query.all()
     all_personajes=list(map(lambda x: x.serialize(), personajes))
     return jsonify(all_personajes), 200
 
 #OBTENGO 1 PERSONAJE
-@app.route("/personajes/<int:persoanje_id", method=['GET'])
+@app.route("/personajes/<int:id>", methods=['GET'])
 def personaje(id):
     personaje= Personajes.query.get(id)
     return jsonify(personaje), 200
 
 #OBTENER PLANETAS
-@app.route("/planetas", method=['GET'])
+@app.route("/planetas", methods=['GET'])
 def planetas_list():
     planetas= Planetas.query.all()
     all_planetas=list(map(lambda x: x.serialize(), planetas))
     return jsonify(all_planetas), 200
+
+#OBTENGO 1 PLANETA
+@app.route("/planetas/<int:id>", methods=['GET'])
+def planeta(id):
+    planeta= Planetas.query.get(id)
+    return jsonify(planeta), 200
+
+#OBTENGO TODOS LOS USERS
+@app.route("/users", methods=['GET'])
+def users_list():
+    users= User.query.all()
+    all_users=list(map(lambda x: x.serialize(), users))
+    return jsonify(all_users), 200
+
+#OBTENGO 1 USER
+@app.route("/users/<int:id>", method=['GET'])
+def user_list(id):
+    user= User.query.get(id)
+    one_users=list(map(lambda x: x.serialize(), user))
+    return jsonify(one_users), 200
+
+
+# AGREGAR UN PLANETA A FAVORITOS
+
+@app.route("/favoritos/planetas/<int:id>", methods=['POST'])
+def agregar_planeta(id):
+    planeta = Planetas.query.get(id)
+    if not planeta:
+        return jsonify({"error": "Planeta no encontrado"}), 400
+    
+    favorito_existente = Favoritos.query.filter_by(planet_id=id).first()
+    if favorito_existente:
+        return jsonify({"error": "El planeta ya está en la lista de favoritos"}), 400
+    nuevo_favorito = Favoritos(planet_id=id)
+
+    db.session.add(nuevo_favorito)
+    db.session.commit()
+    return jsonify({"message": f"El planeta {planeta.nombre} ha sido añadido a tus favoritos."}), 200
+
+# AGREGAR 1 PERSONAJE A FAVORITOS
+
+@app.route("/favoritos/personajes/<int:id>", methods=['POST'])
+def agregar_personaje(id):
+
+
+
+
+#ELIMINAR 1 PLANETA DE FAVORITOS
+
+@app.route("/favoritos/planetas/<int:id>", methods=['DELETE'])
+def eliminar_planeta(id):
+    planeta = Planetas.query.get(id)
+    db.session.delete(planeta)
+    db.session.commit()
+
+
+#ELIMINAR 1 PERSONAJE DE FAVORITOS
+
+@app.route("/favoritos/personajes/<int:id>", methods=['DELETE'])
+def eliminar_personaje(id):
+    personaje = Personajes.query.get(id)
+    db.session.delete(personaje)
+    db.session.commit()
 
 #OBTENER FAVORITOS
 @app.route("/favoritos", method=['GET'])
